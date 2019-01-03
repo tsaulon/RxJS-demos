34974989880295be5cb4c382369bf7b1d3f66ba8
Tyrone Saulon <tyrone.saulon@hotmail.com>
2019-01-02 20:25:33 -0500

    Reducing coupling for hot observable

--- hotObservable.js
+++ hotObservable.js
@@ -1,6 +1,9 @@
 var rxjs_1 = require('rxjs');
+var operators_1 = require('rxjs/operators');
 //  Hot Observables are observables where data is created outside the observable
-var number = Math.random();
-var hot = rxjs_1.Observable.create(function (observer) { return observer.next(number); });
+//  let number = Math.random();
+var cold = rxjs_1.Observable.create(function (observer) { return observer.next(Math.random()); });
+var hot = cold.pipe(operators_1.publish());
 var firstSub = hot.subscribe(console.log);
 var secondSub = hot.subscribe(console.log);
+hot.connect();
--- hotObservable.ts
+++ hotObservable.ts
@@ -1,8 +1,12 @@
 import { Observable } from 'rxjs';
+import { publish } from 'rxjs/operators';
 
 //  Hot Observables are observables where data is created outside the observable
-let number = Math.random();
-let hot = Observable.create(observer => observer.next(number));
+//  let number = Math.random();
+let cold = Observable.create(observer => observer.next(Math.random()));
+let hot = cold.pipe(publish());
 
 let firstSub = hot.subscribe(console.log);
-let secondSub = hot.subscribe(console.log);
+let secondSub = hot.subscribe(console.log); 
+
+hot.connect();
